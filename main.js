f11d5152ad364ca70b6621372e6606351017b70d
0^1 <116479360+0ToThe1@users.noreply.github.com>
2022-10-26 22:38:39 -0700

    Changing Clicking System

--- main.js
+++ main.js
@@ -2,6 +2,7 @@
 * Code is not well written which is kind of evident.
 * Everything is being written in one class cuz why not??
 * It's just a Clicker Game, why would I make it have seamless code anyway?
+* You aren't supposed to see this anyway! Just me.
 */
 
 // GENERAL SETTINGS
@@ -27,13 +28,13 @@ function setup() {
   
   console.log("WELCOME TO CETYS CLICKER!");
   
-  assignButton();
+  // assignButton();
 
   clicker();
 }
 
-var cetysX=0;
-var cetysY=0;
+var cetysX=0, cetysY=0;
+var shopButtonX=0, shopButton=0;
 
 function draw() {
   gameWidth = windowWidth;
@@ -58,31 +59,33 @@ function draw() {
 }
 
 function gui() {
+  // Top Yellow Bar
   textAlign(CENTER);
   fill(color(255, 204, 0))
   strokeWeight(0);
   rect(0, 0, gameWidth, 50);
   
+  // Cetys Clicker text
   textSize(25);
   fill(color(100, 100, 100));
   text("CETYS CLICKER", gameWidth/2, gameHeight-10);
-
-  textAlign(CENTER);
+  
+  // MONEY
   textSize(40);
   fill(color(256, 256, 256));
   text(clicks, (gameWidth)/2, 40);
 
+  // CREDITS
   textSize(20);
   fill(color(100, 100, 100));
   text("By: 0^1", 40, gameHeight-10);
 
   // SHOP BUTTON
-  // rect(0, 0, 50, 50);
-  // textSize(33);
-  // fill(color(256, 256, 256));
-  // text("$", 24, 35);
-
-  assignButton(0, 0, 50, 50, 0, 0);
+  // assignButton(0, 0, 50, 50, 0, 0);
+  // Button(0, 0, 50, 50, 0, 0);
+  textSize(33);
+  fill(color(256, 256, 256));
+  text("$", 24, 35);
 
   shop();
 }
@@ -102,31 +105,42 @@ var cetysSizeOffsetter = 0;
 
 var clickerTime = 0;
 function clicker() {
-  if(pressMouseOnImage((gameWidth-250)/2, (gameHeight-300)/2, cetysButton)) {
-    clickerTime++;
-    if(clickerTime == 1) {
-      clicks++;
-      cetysSizeOffsetter = 20;
-    }
-  } else {
-    clickerTime = 0;
-    cetysSizeOffsetter = 0;
-  }
-}
-
-var button;
-function assignButton(x, y, width, height, color, button) {
-  var sizeOffset = 0;
-  rect(x, y, width, height);
-
-  // if(pressMouseOnImage(x - sizeOffset, y - sizeOffset, )) {
-
+  // if(pressMouseOnImage((gameWidth-250)/2, (gameHeight-300)/2, cetysButton.width/4, cetysButton.height/4)) {
+  //   clickerTime++;
+  //   if(clickerTime == 1) {
+  //     clicks++;
+  //     cetysSizeOffsetter = 20;
+  //   }
+  // } else {
+  //   clickerTime = 0;
+  //   cetysSizeOffsetter = 0;
   // }
 }
 
+class Button {
+  button = 0;
+  clickTime = 0;
+  sizeOffset = 0;
+  isClicked = false;
+  constructor(x, y, width, height, color, button) {
+    rect(x+(sizeOffset/2), y+(sizeOffset/2), width-sizeOffset, height-sizeOffset);
+  
+    if(pressMouseOnImage(x - sizeOffset, y - sizeOffset, width, height)) {
+      clickTime++;
+      if(clickTime == 1) {
+        isClicked = true;
+        sizeOffset = 3;
+      }
+    } else {
+      clickTime = 0;
+      sizeOffset = 0;
+    }
+  }
+}
+
 // Weird looking ass code.
-function pressMouseOnImage(containerX, containerY, container) {  
-  if(mouseX <= containerX + container.width/4 && mouseX >= containerX && mouseY <= containerY + container.height/4 && mouseY >= containerY && mouseIsPressed) return true;
+function pressMouseOnImage(containerX, containerY, containerWidth, containerHeight) {  
+  if(mouseX <= containerX + containerWidth && mouseX >= containerX && mouseY <= containerY + containerHeight && mouseY >= containerY && mouseIsPressed) return true;
   return false;
 }
 
